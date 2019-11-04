c2344b96f4c047cb74f6215dd9303cf1b8bd0c8f
Eduardo Orive <edorka@gmail.com>
2019-11-04 16:13:11 +0100

    added tests to request project deletion via JSON

--- src/features/projects/list-item.js
+++ src/features/projects/list-item.js
@@ -1,48 +1,52 @@
 import { html, css, LitElement } from 'lit-element';
 
+const APIHost = 'http://localhost:3000';
+
 export class ProjectListItem extends LitElement {
   static get styles() {
     return css`
-        :root {
-            display: inline;
-        }
-        .row {
-            opacity: 1;
-            max-height: 50px; 
-            overflow: hidden;
-        }
-        .row.appears {
-            opacity: 0;
-            max-height: 0;
-        }
-        .row, .row.appears {
-            transition: all 1s linear;
-        }
-        .title, .control{
-            display: inline;
-            line-height: 2em;
-            vertical-align: middle;
-        }
-        .title {
-            font-size: 2em;
-        }
-        .control {
-            background: transparent;
-            border-radius: 4em;
-            border: solid 1px grey;
-            height: 2em;
-            width: 2em;
-        }
-        .row .control {
-            opacity: 0;
-        }
-        .row:hover .control {
-            opacity: 1;
-        }
-        .row .control,
-        .row:hover .control {
-            transition: all .5s ease-in-out;
-        }
+      :root {
+        display: inline;
+      }
+      .row {
+        opacity: 1;
+        max-height: 50px;
+        overflow: hidden;
+      }
+      .row.appears {
+        opacity: 0;
+        max-height: 0;
+      }
+      .row,
+      .row.appears {
+        transition: all 1s linear;
+      }
+      .title,
+      .control {
+        display: inline;
+        line-height: 2em;
+        vertical-align: middle;
+      }
+      .title {
+        font-size: 2em;
+      }
+      .control {
+        background: transparent;
+        border-radius: 4em;
+        border: solid 1px grey;
+        height: 2em;
+        width: 2em;
+      }
+      .row .control {
+        opacity: 0;
+      }
+      .row:hover .control {
+        opacity: 1;
+      }
+      .row .control,
+      .row:hover .control {
+        transition: all 0.5s ease-in-out;
+      }
     `;
   }
 
@@ -53,7 +57,7 @@ export class ProjectListItem extends LitElement {
     };
   }
   firstUpdated() {
-    setTimeout( () => {
+    setTimeout(() => {
       const row = this.shadowRoot.querySelector('.row');
       row.classList.remove('appears');
     });
@@ -64,11 +68,45 @@ export class ProjectListItem extends LitElement {
     this.title = '';
     this.children = [];
   }
-
-  remove(event) {
-    const detail = { target: this };
+  async __receiveResponse(response) {
+    let result = null;
+    try {
+      const confirmation = await response.json();
+      if (confirmation.error === true) {
+        throw confirmation;
+      }
+      result = { done: true, target: this };
+    } catch (error) {
+      const report =
+        error instanceof SyntaxError
+          ? undefined
+          : error.reason !== undefined
+          ? error.reason
+          : undefined;
+      this.error = report || "Can't confirm deletion";
+      result = { done: false, error: this.error, target: this };
+    }
     const bubbles = true;
-    this.dispatchEvent(new CustomEvent('project-removed', {bubbles, detail}) );
+    const resultEvent = new CustomEvent('project-removed', {
+      bubbles,
+      detail: result,
+    });
+    this.dispatchEvent(resultEvent);
+    this.requestUpdate();
+  }
+
+  __deleteProject(id) {
+    this.error = null;
+    fetch(APIHost + '/projects/' + id, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(response => this.__receiveResponse(response));
+  }
+  remove(event) {
+    this.__deleteProject(this.id);
+    return false;
   }
 
   render() {
--- src/features/projects/list-item.test.js
+++ src/features/projects/list-item.test.js
@@ -1,12 +1,17 @@
 import { html, fixture, expect } from '@open-wc/testing';
 import { elementUpdated, oneEvent } from '@open-wc/testing-helpers';
-
-import '../../../src/features/projects/list-item.js';
-
+import fetchMock from 'fetch-mock';
+fetchMock.config.sendAsJson = true;
+const APIHost = 'http://localhost:3000';
+import './list-item.js';
 
 describe('<projects-list-item>', () => {
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
 
-  it('should show project\'s title', async () => {
+  it("should show project's title", async () => {
     const el = await fixture(html`
       <project-list-item .title=${'Sample project'}></project-list-item>
     `);
@@ -14,15 +19,74 @@ describe('<projects-list-item>', () => {
     expect(obtained.textContent).to.equal('Sample project');
   });
 
-  it('should request project deletion', async () => {
-    const emptyProjects = [];
+  it('should confirm project deletion', async () => {
+    const done = {
+      done: true,
+      id: 1,
+    };
+    const saved = new Response(JSON.stringify(done), { status: 200 });
+    const mock = fetchMock.delete(APIHost + '/projects/1', saved);
     const el = await fixture(html`
-      <project-list-item .title=${'Sample project'}></project-list-item>
+      <project-list-item .id=${1} .title=${'Sample project'}></project-list-item>
     `);
     const removeButton = el.shadowRoot.querySelector('.row .remove');
     const falseClickEvent = new Event('click', { bubbles: true });
+    await mock.flush();
     setTimeout(() => removeButton.dispatchEvent(falseClickEvent));
     const { detail } = await oneEvent(el, 'project-removed');
+    expect(detail.target.title).to.equal('Sample project');
+  });
+
+  it('should report failure reason during deletion', async () => {
+    const done = {
+      error: true,
+      reason: 'project not found',
+    };
+    const saved = new Response(JSON.stringify(done), { status: 404 });
+    const mock = fetchMock.delete(APIHost + '/projects/1', saved);
+    const el = await fixture(html`
+      <project-list-item .id=${1} .title=${'Sample project'}></project-list-item>
+    `);
+    const removeButton = el.shadowRoot.querySelector('.row .remove');
+    const falseClickEvent = new Event('click', { bubbles: true });
+    await mock.flush();
+    setTimeout(() => removeButton.dispatchEvent(falseClickEvent));
+    const { detail } = await oneEvent(el, 'project-removed');
+    expect(detail.error).to.equal('project not found');
+    expect(detail.target.title).to.equal('Sample project');
+  });
+
+  it('should report failure of unknown reason during deletion', async () => {
+    const error = {
+      error: true,
+    };
+    const saved = new Response(JSON.stringify(error), { status: 404 });
+    const mock = fetchMock.delete(APIHost + '/projects/1', saved);
+    const el = await fixture(html`
+      <project-list-item .id=${1} .title=${'Sample project'}></project-list-item>
+    `);
+    const removeButton = el.shadowRoot.querySelector('.row .remove');
+    const falseClickEvent = new Event('click', { bubbles: true });
+    await mock.flush();
+    setTimeout(() => removeButton.dispatchEvent(falseClickEvent));
+    const { detail } = await oneEvent(el, 'project-removed');
+    expect(detail.error).to.equal("Can't confirm deletion");
+    expect(detail.target.title).to.equal('Sample project');
+  });
+
+  it('should report failure of unknown reason on response parsing', async () => {
+    const error = '<div>Gateway error 502</div>';
+    const saved = new Response(error, { status: 404 });
+    const mock = fetchMock.delete(APIHost + '/projects/1', saved);
+    const el = await fixture(html`
+      <project-list-item .id=${1} .title=${'Sample project'}></project-list-item>
+    `);
+    const removeButton = el.shadowRoot.querySelector('.row .remove');
+    const falseClickEvent = new Event('click', { bubbles: true });
+    await mock.flush();
+    setTimeout(() => removeButton.dispatchEvent(falseClickEvent));
+    const { detail } = await oneEvent(el, 'project-removed');
+    expect(detail.error).to.equal("Can't confirm deletion");
     expect(detail.target.title).to.equal('Sample project');
   });
 });
